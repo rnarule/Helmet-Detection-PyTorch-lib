7efa07269be8ee2372d886f9019d2cc0e2bd448f
rnarule <rnarule@gmail.com>
2024-03-02 10:16:42 +0530

    data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet",
+    version="0.0.0",
+    author="rnarule",
+    author_email="rnarule@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
